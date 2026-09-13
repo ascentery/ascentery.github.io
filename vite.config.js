c445e1938054f8b62dbacff997522321197f9ec5
Psyblade <p5yb14d3@gmail.com>
2026-09-13 12:03:23 +0800

    Enable sourcemaps to diagnose the follower-list crash

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  build: { outDir: 'dist', sourcemap: false },
+  build: { outDir: 'dist', sourcemap: true },
 })
